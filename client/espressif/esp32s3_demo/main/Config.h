e92cfc2ec9cb383f6f41675e85bcbbabe6d0d055
yuhuawei <yuhuawei.123@bytedance.com>
2025-04-10 23:30:30 +0800

    feat: update config

--- client/espressif/esp32s3_demo/main/Config.h
+++ client/espressif/esp32s3_demo/main/Config.h
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 // 服务端的地址
-#define DEFAULT_SERVER_HOST "127.0.0.1:8080"
+#define DEFAULT_SERVER_HOST "127.***.1:8080"
 
 // 默认的智能体id
 #define DEFAULT_BOT_ID "ep-20240729********"
