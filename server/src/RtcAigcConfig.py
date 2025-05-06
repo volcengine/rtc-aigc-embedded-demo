b5d13279bfef80fa9b99041de531979d8956e851
yuhuawei <yuhuawei.123@bytedance.com>
2025-05-06 15:46:27 +0800

    feat: comment server config

--- server/src/RtcAigcConfig.py
+++ server/src/RtcAigcConfig.py
@@ -1,16 +1,24 @@
 # Copyright (2025) Beijing Volcano Engine Technology Ltd.
 # SPDX-License-Identifier: MIT
 
-SK = ""
-AK = ""
+# 鉴权 AK/SK。前往 https://console.volcengine.com/iam/keymanage 获取
+SK = "WmpCbVl6Y3hOR1JrT************1tTTRZalF4WW1FeE56WQ=="
+AK = "AKLTNWQyODQ1MDM5Y***********WRmM2Y2NTJlMTQyZjI"
 
-RTC_APP_ID = ""
-RTC_APP_KEY = ""
+# 实时音视频 App ID。前往 https://console.volcengine.com/rtc/listRTC 获取或创建
+RTC_APP_ID = "67582ac8******0174410bd1"
+# 实时音视频 APP KEY。前往 https://console.volcengine.com/rtc/listRTC 获取
+RTC_APP_KEY = "1a6a03723c******222ada877ee13b"
 
-DEFAULT_BOT_ID = ""
-DEFAULT_VOICE_ID = ""
+# 大模型推理接入点 EndPointId 前往 https://console.volcengine.com/ark/region:ark+cn-beijing/endpoint?config=%7B%7D 创建
+DEFAULT_BOT_ID = "ep-2025******160517-hlnzt"
+# 音频生成-语音合成 Voice_type，前往 https://console.volcengine.com/speech/service/8 获取
+DEFAULT_VOICE_ID = "BV007_******ming"
 
-ASR_APP_ID = ""
-TTS_APP_ID = ""
+# 语音识别-流式语音识别 APPID 前往 https://console.volcengine.com/speech/service/16 获取
+ASR_APP_ID = "884***621"
+# 音频生成-语音合成 APPID，前往 https://console.volcengine.com/speech/service/8 获取
+TTS_APP_ID = "884***9621"
 
+# 服务端监听端口号,你可以根据实际业务需求设置端口号
 PORT = 8080
